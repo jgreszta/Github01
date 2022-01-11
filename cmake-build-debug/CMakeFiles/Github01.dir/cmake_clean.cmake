9cea0abd46c8b69e65f0875423d360396350bbec
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2022-01-11 21:36:39 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Github01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Github01.dir/main.cpp.o"
+  "Github01"
+  "Github01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Github01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
